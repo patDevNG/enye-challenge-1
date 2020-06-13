bcbe3c17b2837f01e994a17a8694d4214a0a44be
Patrick Okuns <cgrleadership@gmail.com>
2020-06-13 21:22:55 +0100

    updated the UI for challenge 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import {
     ComboboxOption
 } from "@reach/combobox";
 
-import {List, Slider, Col, Row, Typography, Space, notification, Spin, Button,  } from 'antd';
+import {List, Slider, Col, Row, Typography, Space, notification, Spin, Button, AutoComplete } from 'antd';
 import { CarFilled, StarFilled } from '@ant-design/icons';
 
 import "@reach/combobox/styles.css";
@@ -33,22 +33,23 @@ const IconText = (params: any) => {
 
 const  App = () => {
     
-    const {
-        ready,
-        value,
-        suggestions: {status, data},
-        setValue,
-        clearSuggestions
-    } = usePlacesAutocomplete();
+    // const {
+    //     ready,
+    //     value,
+    //     suggestions: {status, data},
+    //     setValue,
+    //     clearSuggestions
+    // } = usePlacesAutocomplete();
     const [radius, setRadius] = useState<number>(4000);
     const [baseLocation, setBaseLocation] = useState({
         lat: 0,
         lng: 0,
     });
     const [isLoading, setIsLoading] = useState(false);
+    const [isLoadingSearchHistory, setIsLoadingSearchHistory] = useState(false);
     const [address, setAddress] = useState('');
     const [name, setName] = useState('hospital');
-  
+    const [val, setValue] = useState('')
     const [hospitals, setHospitals] = useState<hospitalData[]>([]);
     const [searchHistory,setSearchHistory] = useState<SearchHistory[]>([])
     const { lat, lng } = baseLocation;
@@ -105,17 +106,48 @@ const  App = () => {
 
     }, [lat, lng, radius,name]);
 
+    useEffect(()=>{
+        const handleSearchHistory = async()=>{
+            const parameter: RequestInit = {
+                method: 'GET',
+                mode: 'cors',
+            } 
+            const url =`http://localhost:5001/okuns-enye-challenge1/us-central1/api`;
+            try {
+                setIsLoadingSearchHistory(true);
+               const response = await fetch('https://us-central1-okuns-enye-challenge1.cloudfunctions.net/api',parameter);
+               const res = await response.json(); 
+               
+               if(res.status === 200){
+                   setIsLoadingSearchHistory(false)
+                setSearchHistory(res.data);
+            
+               }
+            } catch (error) {
+                setIsLoadingSearchHistory(false);
+               console.log(error)
+               notification.error(
+                  {
+                    message:"error",
+                    description:'Something went wrong, try again'
+                  }
+               ) 
+            }
+        }
+        handleSearchHistory();
+    },[])
+
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
         setValue(e.target.value)
-        if(e.target.value.toLowerCase() ==='hospital'||e.target.value.toLowerCase() ==='clinics'|| e.target.value.toLowerCase() ==='pharmacy'|| e.target.value.toLowerCase() ==='medical office'){
-            setAddress(e.target.value)
-            handleUserLocation();
-            setName(e.target.value);
-            clearSuggestions();
-        }else{
-            // setValue(e.target.value);
-            setName('hospital')
-        }
+        // if(e.target.value.toLowerCase() ==='hospital'||e.target.value.toLowerCase() ==='clinics'|| e.target.value.toLowerCase() ==='pharmacy'|| e.target.value.toLowerCase() ==='medical office'){
+        //     setAddress(e.target.value)
+        //     handleUserLocation();
+        //     setName(e.target.value);
+        //     clearSuggestions();
+        // }else{
+        //     // setValue(e.target.value);
+        //     setName('hospital')
+        // }
     };
     
     const handleChange= (sliderValue: any) =>{
@@ -124,16 +156,19 @@ const  App = () => {
 
     const handleSelect = async (val: string): Promise<void> => {
         try {
-            setValue(val, false);
-            const results = await getGeocode({address: val});
-            setAddress(val);
-                const {lat, lng} = await getLatLng(results[0]);
+            // setValue(val, false);
+            // const results = await getGeocode({address: val});
+            // setAddress(val);
+            handleUserLocation();
+                // const {lat, lng} = await getLatLng(results[0]);
                 console.log(lat,lng);
-                setName('hospitals');
-                setBaseLocation({
-                    lat,
-                    lng
-            })
+                setName(val);
+                setAddress(val);
+                setValue(val)
+            //     setBaseLocation({
+            //         lat,
+            //         lng
+            // })
            
         } catch (error) {
             notification.error({
@@ -142,18 +177,19 @@ const  App = () => {
               });
         } 
     };
+   
 
-    const renderSuggestions = (): JSX.Element => {
-        const possiblePlaces = data.map(({id, description}: any) => (
-            <ComboboxOption key={id} value={description}/>
-        ));
+    // const renderSuggestions = (): JSX.Element => {
+    //     const possiblePlaces = data.map(({id, description}: any) => (
+    //         <ComboboxOption key={id} value={description}/>
+    //     ));
 
-        return (
-            <>
-                {possiblePlaces}
-            </>
-        );
-    }; 
+    //     return (
+    //         <>
+    //             {possiblePlaces}
+    //         </>
+    //     );
+    // }; 
     const  formatter= (value: any): string => {
         return `${value}km`;
     }
@@ -168,7 +204,7 @@ const  App = () => {
             return ( hospitals.length > 0 && <List
                 itemLayout="vertical"
                 pagination={{
-                    pageSize: 5,
+                    pageSize: 3,
                   }}
                 dataSource={hospitals}
                 renderItem={item => {
@@ -194,15 +230,7 @@ const  App = () => {
             />);
         }
     } 
-    // const { Option } = Select;
-
-    // const renderSelectOptions: () => JSX.Element[] = () => {
-    //     return optionValues.map(({key, value}) => <Option value={value} key={key}>{value}</Option>)
-    // }
-
-    // const handleMultipleSelect: any = (value: any) => {
-    //     setName(value);
-    // }
+   
     const handleUserLocation =() =>{
         navigator.geolocation.getCurrentPosition((position:Position)=>{
             setBaseLocation({
@@ -219,88 +247,114 @@ const  App = () => {
         );
     }
     
-    const handleSearchHistory = async()=>{
-        const parameter: RequestInit = {
-            method: 'GET',
-            mode: 'cors',
-        } 
-        const url =`http://localhost:5001/okuns-enye-challenge1/us-central1/api`;
-        try {
-           const response = await fetch('https://us-central1-okuns-enye-challenge1.cloudfunctions.net/api',parameter);
-           const res = await response.json(); 
-           
-           if(res.status === 200){
-            setSearchHistory(res.data);
-           }
-        } catch (error) {
-           console.log(error)
-           notification.error(
-              {
-                message:"error",
-                description:'Something went wrong, try again'
-              }
-           ) 
-        }
-    }
+    
 
     const renderSeachHistory = (): false | JSX.Element =>{
-        return( searchHistory.length > 0 && <List
-            itemLayout="vertical"
-            // pagination={{
-            //     pageSize: 5,
-            //   }}
-            dataSource={searchHistory}
-            renderItem={item => {
-                const { createdAt, lat,lng, name, address } = item;
-               
-
-                return (
-                    <List.Item
-                    actions={[
-                    //   <IconText icon={<StarFilled style={{color: 'gold'}} />} text={rating.toFixed(1)} key="list-vertical-star-o" />,
-                    //   <IconText icon={<CarFilled style = {{color: 'green'}} />} text={`${formatter(distance)}`} key="list-vertical-like-o" />,
-                    ]}>
-                      <List.Item.Meta 
-                        title= {address}
-                        description={createdAt}/>
-                    </List.Item>
-                  )
-            } }
-        />
-        );
+        if(isLoadingSearchHistory ==true){
+          return  (
+            <div className='d-flex justify-content-center mt-4'>
+            <Spin className='text-center' size='large'/>
+        </div>
+          )
+        }else{
+            return( searchHistory.length > 0 && <List
+                itemLayout="vertical"
+                // pagination={{
+                //     pageSize: 5,
+                //   }}
+                dataSource={searchHistory}
+                renderItem={item => {
+                    const { createdAt, lat,lng, name, address } = item;
+                   
+    
+                    return (
+                        <List.Item
+                        actions={[
+                        //   <IconText icon={<StarFilled style={{color: 'gold'}} />} text={rating.toFixed(1)} key="list-vertical-star-o" />,
+                        //   <IconText icon={<CarFilled style = {{color: 'green'}} />} text={`${formatter(distance)}`} key="list-vertical-like-o" />,
+                        ]}>
+                          <List.Item.Meta 
+                            title= {<a onClick= {(e)=>{handleSearchCLick(e,lat,lng,name,address)}}>{address}</a>}
+                            description={createdAt}/>
+                        </List.Item>
+                      )
+                } }
+            />
+            );
+        }
+        
     }
-        const handleSearchCLick = (e: React.ChangeEvent<HTMLInputElement>):void=>{
-            setSearchHistory([]);
+        const handleSearchCLick = (event: React.MouseEvent<HTMLElement>, lat:number, lng:number,name:string, address:string):any=>{
+        event.preventDefault();
+        setRadius(4000);
             setBaseLocation({
                 lat:lat,
                 lng:lng
             });
             setAddress(address);
             setName(name);
+
            
         }
+        const searchOptions = [
+            { value: 'hospital' },
+            { value: 'clinics' },
+            { value: 'pharmacy' },
+            { value: 'medical office' }
+          ];
+          const handleInputChange = (data: string) => {
+            setValue(data);
+          };
+          const clearSearchBox =()=>{
+              if(val !== ''){
+                  setValue('');
+              }
+          }
+          const Complete: React.FC = () => (
+            <AutoComplete 
+              style={{ width: 500 }}
+              value={val}
+              options={searchOptions}
+              onSelect ={handleSelect}
+              onChange ={handleInputChange}
+              onClick ={clearSearchBox}
+            //   oclnChange ={handleInput}
+              placeholder=" Search for 'hospitals', 'pharmacy','clinics', 'medical office'"
+              filterOption={(inputValue, option:any) =>
+                option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+              }
+            />
+          );
     return ( 
-        <Row justify='space-between'>
-            <Col sm={24} md={20} lg={20}>
-                <div className='search-area'>
-                <Typography.Title className='text-center mt-4' level={2}>
+        <div className="container">
+            <Row justify='space-between'gutter={[32,32]}>
+            <Col sm={24} md={20} lg={16}>
+                
+                <Typography.Title className='mt-4 ' level={2}>
                     Stay Safe, Stay Calm
                 </Typography.Title>
-                </div>
+                
 
-                <div className='my-2 d-flex justify-content-center'>
+                <div className='my-2 '>
                     <Combobox onSelect={handleSelect} >
-                        <ComboboxInput className ="input" placeholder='Search for the hospitals near you....'
+                        <ComboboxInput className ="input" placeholder='Search for the hospitals, medical offices,pharmacy and clinics near you...'
                             style={{ width: 500, maxWidth: "90%" }}
-                            value={value}
+                            value={val}
                             onChange={handleInput}
-                            disabled={!ready}
+                            selectOnClick = {true}
+                            // disabled={!ready}
                             
                         />
                         <ComboboxPopover>
-                            <ComboboxList>{status === "OK" && renderSuggestions()}</ComboboxList>
+                            <ComboboxList>
+                                <ComboboxOption value='pharmacy'/>
+                                <ComboboxOption value='hospital'/>
+                                <ComboboxOption value='clinics'/>
+                                <ComboboxOption value='medical office'/>
+                            </ComboboxList>
                         </ComboboxPopover>
                     </Combobox>
+                    {/* <Complete/> */}
                 </div>
 
                 {/* <Select
@@ -313,7 +367,7 @@ const  App = () => {
                     {renderSelectOptions()}
                 </Select>  */}
 
-                <div className='d-flex justify-content-center'>
+                <div >
                    <p className='mr-3'>  Choose range in km:</p>
                    <Slider style={{ width: 250 }} onChange={handleChange} tipFormatter={formatter} min={4} max={20} step={1} />   
                 </div>
@@ -321,12 +375,14 @@ const  App = () => {
                 {renderList()}
 
             </Col>
-            <Col lg={4}>
+            <Col lg={8} className ='mt-5'>
                 
-                <Button className ='view-history' type='primary' onClick={handleSearchHistory}>View Search History</Button>
+                <Typography.Title level={1}>Search History</Typography.Title>
                 {renderSeachHistory()}
             </Col>
         </Row> 
+        </div>
+        
     );
 }
 export default App;
